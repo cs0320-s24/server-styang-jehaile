cedc3191f1efc2b06d63205f16a2990e98f0c605
simontyang <thibeaultsy@gmail.com>
2024-02-11 13:08:44 -0500

    feat: broadband handler

--- src/main/java/edu/brown/cs/student/main/Server/BroadbandHandler.java
+++ src/main/java/edu/brown/cs/student/main/Server/BroadbandHandler.java
@@ -1,13 +1,33 @@
 package edu.brown.cs.student.main.Server;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import spark.Request;
 import spark.Response;
 import spark.Route;
 
 public class BroadbandHandler implements Route {
 
+  public BroadbandHandler() {
+    HttpRequest stateToCodeRequest =
+        HttpRequest.newBuilder()
+            .uri(new URI("https://api.census.gov/data/2010/dec/sf1?get=NAME&for=state:*"))
+            .GET()
+            .build();
+
+    // Send that API request then store the response in this variable. Note the generic type.
+    HttpResponse<String> sentBoredApiResponse =
+        HttpClient.newBuilder()
+            .build()
+            .send(buildBoredApiRequest, HttpResponse.BodyHandlers.ofString());
+  }
   @Override
   public Object handle(Request request, Response response) {
     String targetState = request.queryParams("state");
     String targetCounty = request.queryParams("county");
+
+
+
   }
