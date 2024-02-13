8aab9cf3374276c56fdbed172f93ce60f27dd266
Jowet <jowet@jowets-air-1039.devices.brown.edu>
2024-02-13 10:42:00 -0500

    feat:diff implementations for caching

--- src/main/java/edu/brown/cs/student/main/Server/CachingBroadbandHandler.java
+++ src/main/java/edu/brown/cs/student/main/Server/CachingBroadbandHandler.java
@@ -1,54 +1,87 @@
-//package edu.brown.cs.student.main.Server;
+package edu.brown.cs.student.main.Server;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.concurrent.TimeUnit;
+import spark.Request;
+import spark.Response;
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+
+public class CachingBroadbandHandler implements BroadbandHandlerGeneric {
+  private final BroadbandHandlerGeneric toWrap;
+  private final LoadingCache<String, Collection<String>> cache;
+
+
+  public CachingBroadbandHandler(BroadbandHandlerGeneric toWrap) {
+    this.toWrap = toWrap;
+
+
+    // Look at the docs -- there are lots of builder parameters you can use
+    //   including ones that affect garbage-collection (not needed for Server).
+    this.cache = CacheBuilder.newBuilder()
+        // How many entries maximum in the cache?
+        .maximumSize(5)
+        // How long should entries remain in the cache?
+        .expireAfterWrite(10, TimeUnit.MINUTES)
+        // Keep statistical info around for profiling purposes
+        .recordStats()
+        .build(
+            // Strategy pattern: how should the cache behave when
+            // it's asked for something it doesn't have?
+                new CacheLoader<String, Collection<String>>() {
+                    private Request request;
+                    private Response response;
+                    @Override
+                    public Collection<String> load(String key) throws Exception {
+                        // Implement logic to load data associated with the key
+//                        Request request = new Request(); // You need to create a Request object here
+//                        Response response = new Response(); // You need to create a Response object here
+//                        return (Collection<String>) toWrap.handle(request, response);
+//                        return (Collection<String>) toWrap.handle(new Request(), new Response())
+                        return (Collection<String>) toWrap.handle(this.request, this.response);
 //
-//import java.io.IOException;
-//import java.net.URISyntaxException;
-//import java.util.Collection;
-//import java.util.concurrent.TimeUnit;
-//import spark.Request;
-//import spark.Response;
-//
-//import com.google.common.cache.CacheBuilder;
-//import com.google.common.cache.CacheLoader;
-//import com.google.common.cache.LoadingCache;
-//
-//public class CachingBroadbandHandler implements BroadbandHandlerGeneric {
-//  private final BroadbandHandlerGeneric toWrap;
-//  private final LoadingCache<String, Collection<String>> cache;
-//
-//  public CachingBroadbandHandler(BroadbandHandlerGeneric toWrap) {
-//    this.toWrap = toWrap;
-//
-//
-//    // Look at the docs -- there are lots of builder parameters you can use
-//    //   including ones that affect garbage-collection (not needed for Server).
-//    this.cache = CacheBuilder.newBuilder()
-//        // How many entries maximum in the cache?
-//        .maximumSize(5)
-//        // How long should entries remain in the cache?
-//        .expireAfterWrite(10, TimeUnit.MINUTES)
-//        // Keep statistical info around for profiling purposes
-//        .recordStats()
-//        .build(
-//            // Strategy pattern: how should the cache behave when
-//            // it's asked for something it doesn't have?
+//                        Object result = toWrap.handle(this.request, this.response);
+//                        if (result instanceof Collection) {
+//                            return (Collection<String>) result;
+//                        } else {
+//                            return Collections.emptyList();
+//                        }
+                    }
+                });
+  }
 //            new CacheLoader<Request, Response>() {
-////                @Override
-////                public Response load(Request request) throws Exception {
-////                    return null;
-////                }
-////            @Override
-////              public Response load(String responset) throws URISyntaxException, IOException, InterruptedException {
-////                return null;
-////              }
-//            });
-//  }
-//  @Override
-//  public Object handle(Request request, Response response)
+//                @Override
+//                public Response load(Request request) throws Exception {
 //
-//      throws URISyntaxException, IOException, InterruptedException {
-//      return null;
-//  }
+//                    return null;
+//
+//                }
+//
+//                @Override
+//                public Collection<String> load(String key) throws Exception {
+//                    Request request = new Request(); // You need to create a Request object here
+//                    Response response = new Response(); // You need to create a Response object here
+//                    return (Collection<String>) toWrap.handle(request, response);
+//                }
+//            @Override
+//              public Response load(String responset) throws URISyntaxException, IOException, InterruptedException {
+//                return null;
+//              }
+//            });
+
+
+  @Override
+  public Object handle(Request request, Response response)
+
+      throws URISyntaxException, IOException, InterruptedException {
+      return null;
+  }
 //  public Object handle(Request resquest){
 //      return null;
 //  }
-//}
+}
