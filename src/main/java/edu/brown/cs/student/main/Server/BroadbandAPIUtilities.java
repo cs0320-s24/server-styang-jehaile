90950058c1a3ea3fd04e8b21ced021d3692fe6be
Jowet <jowet@jowets-air-1039.devices.brown.edu>
2024-02-12 19:46:21 -0500

    fix:added error handling

--- src/main/java/edu/brown/cs/student/main/Server/BroadbandAPIUtilities.java
+++ src/main/java/edu/brown/cs/student/main/Server/BroadbandAPIUtilities.java
@@ -8,13 +8,19 @@ public class BroadbandAPIUtilities { // error handling?
   public static BroadbandData deserializeBroadbandData(String jsonBroadbandData)
       throws IOException {
     // Initializes Moshi
-    Moshi moshi = new Moshi.Builder().build();
 
+    Moshi moshi = new Moshi.Builder().build();
+    try{
     // Initializes an adapter to an Activity class then uses it to parse the JSON.
     JsonAdapter<BroadbandData> adapter = moshi.adapter(BroadbandData.class);
 
     BroadbandData broadbandData = adapter.fromJson(jsonBroadbandData);
 
-    return broadbandData;
+    return broadbandData;}
+    catch (IOException e) {
+      throw new IOException("Error deserializing broadband data", e);
+    } catch (Exception e) {
+      throw new IOException("Unexpected error deserializing broadband data", e);
+    }
   }
 }
