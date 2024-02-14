534facd93f855dcd178d4c0ae405e923dc058d84
Jowet <jowet@jowets-air-1039.devices.brown.edu>
2024-02-13 19:37:03 -0500

    feat: added testing

--- src/test/java/ServerTestSuite/ServerTestSuite.java
+++ src/test/java/ServerTestSuite/ServerTestSuite.java
@@ -12,9 +12,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
 
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class ServerTestSuite {
@@ -34,9 +38,9 @@ public class ServerTestSuite {
         serverThread = new Thread(() -> {
             try {
                 Server.main(new String[]{});
+
             } catch (Exception e) {
                 e.printStackTrace();
-                System.out.print("beforeall");
             }
         });
         serverThread.start();
@@ -57,12 +61,6 @@ public class ServerTestSuite {
     @Test
 
     public void testLoadCSV() throws IOException {
-//        LoadCSVHandler loadCSVHandler = new LoadCSVHandler();
-//        Request request = new Request();
-//        loadCSVHandler.handle()
-//        Server server = new Server();
-//        server
-
         URL url = new URL("http://localhost:1234/loadcsv");
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestMethod("GET");
@@ -82,11 +80,6 @@ public class ServerTestSuite {
     }
     @Test
     public void testViewCSV() throws IOException {
-//        LoadCSVHandler loadCSVHandler = new LoadCSVHandler();
-//        Request request = new Request();
-//        loadCSVHandler.handle()
-//        Server server = new Server();
-//        server
 
         URL url = new URL("http://localhost:1234/viewcsv");
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
@@ -107,12 +100,6 @@ public class ServerTestSuite {
     }
     @Test
     public void testSearchCSV() throws IOException {
-//        LoadCSVHandler loadCSVHandler = new LoadCSVHandler();
-//        Request request = new Request();
-//        loadCSVHandler.handle()
-//        Server server = new Server();
-//        server
-
         URL url = new URL("http://localhost:1234/searchcsv");
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestMethod("GET");
@@ -128,10 +115,140 @@ public class ServerTestSuite {
         }
         in.close();
 
+    }
+    @Test
+    public void testViewBeforeLoad() throws IOException {
+
+        String csvFileName = "ten-star.csv";
+        URL url = new URL("http://localhost:1234/viewcsv?fileName=" + csvFileName);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+
+        int responseCode = connection.getResponseCode();
+        assertEquals(200, responseCode);
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        StringBuilder response = new StringBuilder();
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+        System.out.print(response.toString());
+
+        assertTrue(response.toString().contains("Failed to view. Please load CSV file."));
+        }
+    @Test
+    public void testSearchBeforeLoad() throws IOException {
+
+        String csvFileName = "ten-star.csv";
+        URL url = new URL("http://localhost:1234/searchcsv?fileName=" + csvFileName);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+
+        int responseCode = connection.getResponseCode();
+        assertEquals(200, responseCode);
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        StringBuilder response = new StringBuilder();
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+        System.out.print(response.toString());
+
+        assertTrue(response.toString().contains("CSV File not loaded."));
+    }
+    @Test
+    public void testContentsOfViewCSV() throws IOException, InterruptedException {
+        String csvFileName = "ten-star.csv";
+
+        URL loadUrl = new URL("http://localhost:1234/loadcsv?fileName=" + csvFileName);
+        HttpURLConnection loadConnection = (HttpURLConnection) loadUrl.openConnection();
+        loadConnection.setRequestMethod("GET");
+        assertEquals(200, loadConnection.getResponseCode());
+
+        Thread.sleep(3000);
+
+        URL viewUrl = new URL("http://localhost:1234/viewcsv?fileName=" + csvFileName);
+        HttpURLConnection viewConnection = (HttpURLConnection) viewUrl.openConnection();
+        viewConnection.setRequestMethod("GET");
+
+        int responseCode = viewConnection.getResponseCode();
+        assertEquals(200, responseCode);
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(viewConnection.getInputStream()));
+        StringBuilder response = new StringBuilder();
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        //System.out.println("Response: " + response.toString());
+
+        assertTrue(response.toString().contains("StarID"));
+        assertTrue(response.toString().contains("ProperName"));
+        assertTrue(response.toString().contains("X"));
+        assertTrue(response.toString().contains("Y"));
+        assertTrue(response.toString().contains("Z"));
+        assertTrue(response.toString().contains("Sol"));
+        assertTrue(response.toString().contains("Proxima Centauri"));
+        assertTrue(response.toString().contains("-0.01729"));
+        assertTrue(response.toString().contains("\"Data\":[[\"StarID\",\"ProperName\",\"X\",\"Y\",\"Z\"]"));
+    }
+
+
+
+    public void testContentsOfViewCSVLargeData() throws IOException, InterruptedException {
+        String csvFileName = "ten-star.csv";
+
+        URL loadUrl = new URL("http://localhost:1234/loadcsv?fileName=" + csvFileName);
+        HttpURLConnection loadConnection = (HttpURLConnection) loadUrl.openConnection();
+        loadConnection.setRequestMethod("GET");
+        assertEquals(200, loadConnection.getResponseCode());
+
+        Thread.sleep(3000);
+
+        URL viewUrl = new URL("http://localhost:1234/viewcsv?fileName=" + csvFileName);
+        HttpURLConnection viewConnection = (HttpURLConnection) viewUrl.openConnection();
+        viewConnection.setRequestMethod("GET");
+
+        int responseCode = viewConnection.getResponseCode();
+        assertEquals(200, responseCode);
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(viewConnection.getInputStream()));
+        StringBuilder response = new StringBuilder();
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        //System.out.println("Response: " + response.toString());
+
+        assertTrue(response.toString().contains("StarID"));
+        assertTrue(response.toString().contains("ProperName"));
+        assertTrue(response.toString().contains("X"));
+        assertTrue(response.toString().contains("Y"));
+        assertTrue(response.toString().contains("Z"));
+        ;
+
+    }
+    @Test
+    public void testContentsOfSearchCSV(){
+
+    }
+
 
     }
 
 
 
 
-}
+
+
+
+
+
