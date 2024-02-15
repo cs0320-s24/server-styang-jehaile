6750f06b715021ac0a0db4d1a89f97b15f394455
simontyang <thibeaultsy@gmail.com>
2024-02-15 12:29:28 -0500

    test fixed

--- src/main/java/edu/brown/cs/student/main/Server/Broadband/BroadbandData.java
+++ src/main/java/edu/brown/cs/student/main/Server/Broadband/BroadbandData.java
@@ -1,3 +1,9 @@
 package edu.brown.cs.student.main.Server.Broadband;
 
-public record BroadbandData(String state, String county, double percentAccess) {}
+public record BroadbandData(String state, String county, double percentAccess) {
+
+  @Override
+  public String toString() {
+    return "{state=" + state + ", county=" + county + ", percentAccess=" + percentAccess + "}";
+  }
+}
--- src/main/java/edu/brown/cs/student/main/Server/Broadband/BroadbandHandler/BroadbandHandler.java
+++ src/main/java/edu/brown/cs/student/main/Server/Broadband/BroadbandHandler/BroadbandHandler.java
@@ -50,14 +50,14 @@ public class BroadbandHandler implements Route {
 
     responseMap.put("Broadband data:", broadbandData);
 
-    return new BroadbandSuccessResponse(responseMap).serialize();
+    return new BroadbandSuccessResponse(broadbandData).serialize();
 }
 
 public record BroadbandSuccessResponse(
-    String responseType, String dateTime, Map<String, BroadbandData> responseMap) {
+    String responseType, String dateTime, BroadbandData responseData) {
 
-  public BroadbandSuccessResponse(Map<String, BroadbandData> responseMap) {
-    this("Loaded successfully! :)", LocalDateTime.now().toString(), responseMap);
+  public BroadbandSuccessResponse(BroadbandData responseData) {
+    this("Loaded successfully! :)", LocalDateTime.now().toString(), responseData);
   }
 
   String serialize() {
