f6e9273c10d1d2e7308161fa721338e752debb26
simontyang <thibeaultsy@gmail.com>
2024-02-15 00:26:27 -0500

    fix: mock broadband class

--- src/main/java/edu/brown/cs/student/main/Server/Broadband/BroadbandHandler/BroadbandDataSource.java
+++ src/main/java/edu/brown/cs/student/main/Server/Broadband/BroadbandHandler/BroadbandDataSource.java
@@ -7,7 +7,6 @@ import java.net.URISyntaxException;
 import java.net.http.HttpResponse;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.NoSuchElementException;
 
 public class BroadbandDataSource implements BroadbandDataSourceInterface{
--- src/test/java/ServerTestSuite/MockBroadbandSource.java
+++ src/test/java/ServerTestSuite/MockBroadbandSource.java
@@ -1,5 +1,19 @@
 package ServerTestSuite;
 
-public class MockBroadbandSource {
+import edu.brown.cs.student.main.Server.Broadband.BroadbandData;
+import edu.brown.cs.student.main.Server.Broadband.BroadbandHandler.BroadbandDataSourceInterface;
+import java.io.IOException;
+import java.net.URISyntaxException;
 
+public class MockBroadbandSource implements BroadbandDataSourceInterface {
+  private final BroadbandData constantData;
+
+  public MockBroadbandSource(BroadbandData broadbandData) {
+    this.constantData = broadbandData;
+  }
+
+  @Override
+  public BroadbandData getBroadbandData(String state, String county) {
+    return this.constantData;
+  }
 }
